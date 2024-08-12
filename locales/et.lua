a74321c13272db1cf014e53b4fd9ac77c760e66d
Kõpskink <154380199+Kopskink@users.noreply.github.com>
2024-08-13 01:59:54 +0300

    Create et.lua

---
+++ locales/et.lua
@@ -0,0 +1,19 @@
+local Translations = {
+	adhelp = {
+        help = 'Reklaami oma firmat rikkalikult sõnastatud lausetega!',
+		desc = 'Reklaami sisu',
+	},
+    alert = {
+        header = '**REKLAAMIMISE MAKSUMUS!**',
+        socbankcontent = 'Reklaami tegemine sisuga: \n\n**%{text}** \n\n Nõuab teie firma pangakonto arvelt summa **$%{cost}**',
+        urbankcontent = 'Reklaami tegemine sisuga: \n\n**%{text}** \n\n Nõuab teie pangakonto arvelt summa **$%{cost}**',
+    },
+    payment = {
+        explanation = 'Reklaamimise makse.', 
+        socbankpoor = 'Teie firma pangakonto arvel puudub sobiv summa raha, millega katta reklaami kulu.',
+        urbankpoor = 'Teie pangakonto arvel puudub sobiv summa raha, millega katta reklaami kulu.',
+        cancel = '**Mõtlesite ümber...**',
+        adcompany = 'Los Santos Advertisement Company',
+        descr = 'Makse summas $%{cost} firma reklaamimiseks.',
+    },
+}
